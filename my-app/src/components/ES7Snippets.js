f507ed7f9b621716b769e2ea355d5fdebe9c0c5a
LucaGiarr <giarrusso.luca8@gmail.com>
2023-07-08 17:15:19 +0000

    add ES& Snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,64 @@
+// rfce
+// import React from 'react'
+
+// function ES&Snippets() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ES&Snippets
+
+
+
+//rafce
+// import React from 'react'
+
+// const ES&Snippets = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default ES&Snippets
+
+
+
+//rce
+// import React, { Component } from 'react'
+
+// export class ES&Snippets extends Component {
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
+
+// export default ES&Snippets
+
+
+//rconst
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+     
+//   }
+// }
+
+
+//clg
+// console.log(object)
+
+
+//clo
+console.log('object', object)
+
+
